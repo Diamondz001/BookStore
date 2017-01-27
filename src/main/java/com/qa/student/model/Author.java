e224ae3d8e73ffb4cb3ed8916cd3fc1f21b500d5
Diamondz001 <gwatt1337@gmail.com>
2017-01-27 16:17:06 +0000

    Creation of Author, Book and Store Entity

--- src/main/java/com/qa/student/model/Author.java
+++ src/main/java/com/qa/student/model/Author.java
@@ -1,5 +1,105 @@
 package com.qa.student.model;
 
-public class Author {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class Author {
+	// ======================================
+	// = Attributes =
+	// ======================================
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(name = "first_name", nullable = false)
+	private String firstName;
+
+	@Column(name = "second_name", nullable = false)
+	private String secondName;
+
+	// ======================================
+	// = Constructors =
+	// ======================================
+
+	public Author() {
+		}
+
+	public Author(String firstName, String secondName) {
+			this.firstName = firstName;
+			this.secondName = secondName;
+		}
+
+	// ======================================
+	// = Getters & Setters =
+	// ======================================
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	// ===================================
+	// = hashcode, equals & toString = 
+	// ===================================
+	
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", firstName=" + firstName + ", secondName=" + secondName + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((secondName == null) ? 0 : secondName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (secondName == null) {
+			if (other.secondName != null)
+				return false;
+		} else if (!secondName.equals(other.secondName))
+			return false;
+		return true;
+	}	
 }
--- src/main/java/com/qa/student/model/Store.java
+++ src/main/java/com/qa/student/model/Store.java
@@ -1,5 +1,181 @@
 package com.qa.student.model;
 
-public class Store {
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Store {
+	// ======================================
+	// = Attributes =
+	// ======================================
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(name = "store_name")
+	private String name;
+
+	private String address;
+
+	private String postcode;
+
+	private String telephone;
+
+	private String email;
+
+	@OneToMany(cascade = CascadeType.ALL)
+	private Set<Book> books = new HashSet<>();
+
+	// ======================================
+	// = Constructors =
+	// ======================================
+
+	public Store() {
+
+	}
+
+	public Store(String name) {
+		this.name = name;
+	}
+
+	public Store(String name, String address, String postcode) {
+		this.name = name;
+		this.address = address;
+		this.postcode = postcode;
+	}
+
+	// ======================================
+	// = Getters & Setters =
+	// ======================================
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	// ===================================
+	// = hashcode, equals & toString =
+	// ===================================
+
+	@Override
+	public String toString() {
+		return "Store [id=" + id + ", name=" + name + ", address=" + address + ", postcode=" + postcode + ", telephone="
+				+ telephone + ", email=" + email + ", books=" + books + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((books == null) ? 0 : books.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((postcode == null) ? 0 : postcode.hashCode());
+		result = prime * result + ((telephone == null) ? 0 : telephone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Store other = (Store) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (books == null) {
+			if (other.books != null)
+				return false;
+		} else if (!books.equals(other.books))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (postcode == null) {
+			if (other.postcode != null)
+				return false;
+		} else if (!postcode.equals(other.postcode))
+			return false;
+		if (telephone == null) {
+			if (other.telephone != null)
+				return false;
+		} else if (!telephone.equals(other.telephone))
+			return false;
+		return true;
+	}
 }
